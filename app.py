4fa34fa0d4e0785e42e61949a2844ccef5894b4d
Jay Shah <j24shah@gmail.com>
2020-08-29 16:54:54 -0400

    Added in calls for the two scripts

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask
 import logging
+import dht11_test
+import waterlevel
 
 app = Flask(__name__)
+
+@app.route("/")
+def main():
+
+    # calling temperature and humidity script.
+    dht11_test.main()
+
+    # calling water level.
+    adc_value = waterlevel.main()
+
 
 
 
